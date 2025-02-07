fe0cd53dff7c5acc32e1d43ceebafb55c9e726de
AnimGol <minagol91@gmail.com>
2025-02-07 01:08:02 +0100

    Update Database_related.py

--- Database_related.py
+++ Database_related.py
@@ -4,30 +4,30 @@ import sqlite3
 conn = sqlite3.connect("Genius.db") 
 cursor = conn.cursor()
 
-cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "wordcloud for Most Frequent Words" TEXT''')
-cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "wordcloud for nonstop words" TEXT''')
-cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "Barchart for emotion analysis" TEXT''')
+# cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "wordcloud for Most Frequent Words" TEXT''')
+# cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "wordcloud for nonstop words" TEXT''')
+# cursor.execute('''ALTER TABLE "Works Collection" ADD COLUMN "Barchart for emotion analysis" TEXT''')
 
 
 # Check if the table exists
-# cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Works Collection'")
-# table_exists = cursor.fetchone()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Works Collection'")
+table_exists = cursor.fetchone()
 
-# if table_exists:
-#     print("Table exists!")
+if table_exists:
+    print("Table exists!")
     
-#     # Check and print the structure of the table
-#     cursor.execute('PRAGMA table_info("Works Collection")')
-#     columns = cursor.fetchall()
+    # Check and print the structure of the table
+    cursor.execute('PRAGMA table_info("Works Collection")')
+    columns = cursor.fetchall()
     
-#     if columns:
-#         print("Columns in 'Works Collection':")
-#         for column in columns:
-#             print(column)  # Prints column details
-#     else:
-#         print("No columns found in the table.")
-# else:
-#     print("Table does NOT exist.")
+    if columns:
+        print("Columns in 'Works Collection':")
+        for column in columns:
+            print(column)  # Prints column details
+    else:
+        print("No columns found in the table.")
+else:
+    print("Table does NOT exist.")
 
 # Close the connection
 conn.close()
