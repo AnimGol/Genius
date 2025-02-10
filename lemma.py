4fe99f812ce56b4a6d08051079c29707c0cc09d1
Alislash13 <alihmohammad3334@gmail.com>
2025-02-10 20:27:05 +0330

    Update lemma.py

--- lemma.py
+++ lemma.py
@@ -1,4 +1,15 @@
 import spacy
+import os
+print ("Please write the ID (e.g., 10 or 14). Please choose a number that is availavle in the folder of Counts.")
+id = input (). strip ()
+text_folder_path = r"SPGC-counts-2018-07-18"
+file_name = f"PG{id}_counts.txt"
+full_path = os.path.join(text_folder_path, file_name)  # Combine folder and file name
+
+    # Open and read the file
+with open(full_path, "r") as file:
+    content = file.read()
+    print ('file is found and read successfully!')
 
 # Ensure the 'en_core_web_lg' model is installed and load it
 try:
